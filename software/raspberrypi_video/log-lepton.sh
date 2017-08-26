c6aaf5e6bc4278663916aa7e9d93acef628bc0ce
Jon Gips <jon@generalsensing.com>
2017-08-26 15:27:27 -0400

    Added script to log lepton on RPI
    Run this from RPI to start lepton logging and detach process

---
+++ software/raspberrypi_video/log-lepton.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+BASE_ID_FILE=/home/pi/.base_id
+BASE_ID=`cat $BASE_ID_FILE`
+LEPTON_BIN=raspberrypi_video
+LEPTON_BIN_PATH=/home/pi/Documents/LeptonModule/software/raspberrypi_video/$LEPTON_BIN
+
+pkill -f $LEPTON_BIN
+export DISPLAY=:0.0
+nohup $LEPTON_BIN_PATH $BASE_ID &
